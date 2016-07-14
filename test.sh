dd23eed21bd57b4bda824b0b9b541a5dda6673b7
Henry Blyth <blyth.henry@gmail.com>
2016-07-14 11:14:44 +0100

    Add nested generation two-step example with tests

--- test.sh
+++ test.sh
@@ -10,11 +10,27 @@ test_is_directory() {
     fi
 }
 
+test_not_directory() {
+    test -d $1
+    if [ $? -eq 0 ]; then
+        FAIL=1
+        echo "$1 is a directory"
+    fi
+}
+
 test_is_file() {
     test -f $1
     if [ $? -eq 1 ]; then
         FAIL=1
         echo "$1 is not a file"
+    fi
+}
+
+test_not_file() {
+    test -f $1
+    if [ $? -eq 0 ]; then
+        FAIL=1
+        echo "$1 is a file"
     fi
 }
 
@@ -31,12 +47,25 @@ test_files_equal() {
 cookiecutter --no-input .
 
 test_is_directory   myfolder
+
 test_is_file        myfolder/array-choice.txt
 test_is_file        myfolder/object-choice.txt
 test_is_file        myfolder/myfilename.myext
+test_is_file        myfolder/cookiecutter.json
+
 test_files_equal    myfolder/array-choice.txt   expected/array-choice.txt
 test_files_equal    myfolder/object-choice.txt  expected/object-choice.txt
 test_files_equal    myfolder/myfilename.myext   expected/myfilename.myext
+test_files_equal    myfolder/cookiecutter.json  expected/cookiecutter.json
+
+test_not_directory  myfolder/nextfolder
+test_not_file       myfolder/nextfolder/nextfile.txt
+
+cd myfolder && cookiecutter --no-input . && cd ..
+
+test_is_directory   myfolder/nextfolder
+test_is_file        myfolder/nextfolder/nextfile.txt
+test_files_equal    myfolder/nextfolder/nextfile.txt    expected/nextfolder/nextfile.txt
 
 # End
 
